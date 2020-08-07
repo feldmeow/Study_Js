8a76314d476639ec2a4f7f8bda0e04f11e7b34d2
Jauhen Feldman <feldmeow@gmail.com>
2020-08-07 20:12:16 +0300

    Убрал значения переменных

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let money = 1000;
-let income = 1000;
-let addExpenses = 1000;
-let deposit = 1000;
-let mission = 1000;
-let period = 1000;
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
 alert("Hello World!");
 console.log("Hi, dear!");
